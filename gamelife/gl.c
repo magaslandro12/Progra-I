8078d9b9cd0d3e06592bf8e3bfbfc16b19dbb216
Francisco Ledesma <franciscodledesma@gmail.com>
2018-08-29 11:12:01 -0300

    prototipos fuera del main

--- gamelife/gl.c
+++ gamelife/gl.c
@@ -13,33 +13,24 @@ juego!
 
 
 
+
+void initialize_world (void);
+int get_world_width (void);
+int get_world_height (void);
+
+int get_cell_state (int x, int y);
+void set_cell_state (int x, int y, int state );
+void finalize_evolution (void);
+void output_world (void);
+
+
+
 int main(void)
 {
 	int mapa[13][13];
 
 
-void initialize_world (void);
-/* returns the width (x) and height (y) of the world
-*/
-int get_world_width (void);
-int get_world_height (void);
-/* returns the state (DEAD or ALIVE) of the cell at (x,y);
-* coordinates go from x = 0 ,... ,width -1 and
-* y = 0 ,... ,height -1; returns DEAD for cells outside this
-* range
-*/
-int get_cell_state (int x, int y);
-/* sets the state (DEAD or ALIVE) of the cell at (x,y) in
-* the next generation; range of coordinates same as in
-* get_cell_state()
-*/
-void set_cell_state (int x, int y, int state );
-/* updates world state to next generation and resets all
-* next generation states to DEAD
-*/
-void finalize_evolution (void);
-/* outputs the current world state to the console
-*/
-void output_world (void);
-
 }
+
+
+int ()
